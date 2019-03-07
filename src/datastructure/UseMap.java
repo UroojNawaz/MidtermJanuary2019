e6b360f3f54cc8d3da87c2010ee17ea24d7e62c1
UroojNawaz <uroojsnawaz@gmail.com>
2019-03-06 20:43:13 -0500

    Fifth Commit

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,8 +1,22 @@
 package datastructure;
 
+import databases.ConnectToSqlDB;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Iterator;
+
+
 public class UseMap {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		/*
 		 * Demonstrate how to use Map that includes storing and retrieving elements.
 		 * Add List<String> into a Map. Like, Map<String, List<string>> list = new HashMap<String, List<String>>();
@@ -10,6 +24,43 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
+
+		List<String> queensNeighborhoods = new ArrayList<String>();
+		queensNeighborhoods.add("Maspeth");
+		queensNeighborhoods.add("Woodside");
+		queensNeighborhoods.add("Jackson Heights");
+
+		List<String> manhattanNeighborhoods = new ArrayList<String>();
+		manhattanNeighborhoods.add("Herald Square");
+		manhattanNeighborhoods.add("Harlem");
+		manhattanNeighborhoods.add("Washington Heights");
+
+		List<String> bronxNeighborhoods = new ArrayList<String>();
+		bronxNeighborhoods.add("Parkchester");
+		bronxNeighborhoods.add("Pelham Bay");
+		bronxNeighborhoods.add("Fordham");
+
+		List<String> brooklynNeighborhoods = new ArrayList<String>();
+		brooklynNeighborhoods.add("Sheepshedbay");
+		brooklynNeighborhoods.add("Greenpoint");
+		brooklynNeighborhoods.add("Bushwick");
+
+		List<String> statenIslandNeighborhoods = new ArrayList<String>();
+		statenIslandNeighborhoods.add("Tottenville");
+		statenIslandNeighborhoods.add("Brighton Heights");
+		statenIslandNeighborhoods.add("Eltingville");
+
+
+		Map<String, List<String>> fiveBoroughs = new LinkedHashMap<String, List<String>>();
+		fiveBoroughs.put("Queens", queensNeighborhoods);
+		fiveBoroughs.put("Manhattan", manhattanNeighborhoods);
+		fiveBoroughs.put("Bronx", bronxNeighborhoods);
+		fiveBoroughs.put("Brooklyn", brooklynNeighborhoods);
+		fiveBoroughs.put("Staten Island", statenIslandNeighborhoods);
+
+
+		for (Map.Entry<String, List<String>> borough : fiveBoroughs.entrySet())
+			System.out.println("Borough: " + borough.getKey() + " | Neighborhoods: " + borough.getValue());
 
 	}
 
